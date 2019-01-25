ed406468d043218cd678075257e46403c65c49bb
marcorodnav <marcorodnav@gmail.com>
2019-01-24 22:43:41 -0600

    Fix instruction for updating alternatives

--- install_java11.sh
+++ install_java11.sh
@@ -13,5 +13,5 @@ rm -f ~/openjdk-11.0.2_linux-x64_bin.tar.gz
 echo "***********"
 echo ""
 echo "Setting java 11 as default"
-sudo sh -c 'for bin in /usr/lib/jvm/jdk-11/bin/*; do update-alternatives --install /usr/bin/$(basename $bin) $(basename $bin) $bin 100; done'
-sudo sh -c 'for bin in /usr/lib/jvm/jdk-11/bin/*; do update-alternatives --set $(basename $bin) $bin; done'
+sudo sh -c 'for bin in /usr/lib/jvm/jdk-11.0.2/bin/*; do update-alternatives --install /usr/bin/$(basename $bin) $(basename $bin) $bin 100; done'
+sudo sh -c 'for bin in /usr/lib/jvm/jdk-11.0.2/bin/*; do update-alternatives --set $(basename $bin) $bin; done'
